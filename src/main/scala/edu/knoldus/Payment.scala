5142c32bf695f185d3546964d51a9050085975df
Kritikakhandelwal <kritika.khandelwal@knoldus.in>
2018-01-17 06:38:41 +0530

    ScalaAss2-02code

---
+++ src/main/scala/edu/knoldus/Payment.scala
@@ -0,0 +1,34 @@
+package edu.knoldus
+
+import org.apache.log4j.Logger
+
+abstract class PaymentMode {
+
+  case class Paytm() extends PaymentMode
+  case class Freecharge() extends PaymentMode
+  case class NetBanking() extends PaymentMode
+  case class CardPayment() extends PaymentMode
+  case class Cash() extends PaymentMode
+
+}
+
+object Calculator extends PaymentMode with App {
+
+  val log = Logger.getLogger(this.getClass)
+  val amount = 20
+  val payMethod = Paytm()
+  //val totalAmount=print("Total Amount = "+ "Rs" + calculateAmount(amount,Paytm()))
+  val totalAmount = log.info("Total Amount = Rs" + calculateAmount(amount, payMethod))
+
+  def calculateAmount(amount: Int, payMethod: PaymentMode): Double = {
+    payMethod match {
+      case a: Paytm => (amount + (0.02 * amount))
+      case b: Freecharge => (amount + (0.02 * amount))
+      case c: NetBanking => (amount + 5)
+      case d: CardPayment => (amount + 1.5)
+      case e: Cash => (amount)
+    }
+  }
+}
+
+
---
+++ src/main/scala/edu/knoldus/Person1.scala
@@ -0,0 +1,54 @@
+package edu.knoldus
+
+import org.apache.log4j.Logger
+
+abstract class Person1 {
+  def isWinner(num: Int): String = {
+
+    val r = scala.util.Random
+    val digit = 1 + r.nextInt(6)
+    digit match {
+      case 1 | 6 => if (num == 0) "Winner" else isWinner(num - 1)
+      case _ => "Looser"
+
+    }
+  }
+
+  def showPerson(person: Person1): String = {
+    /* def generateRandom(num:Int):Int= {
+       val r = scala.util.Random
+       1 + r.nextInt(6)
+     }*/
+    person match {
+
+      case Trainer() => {
+
+        val strength = scala.util.Random
+        s"Total Attendance =${strength.nextInt(51)}"
+
+      }
+      case Gamer(count: Int) => {
+
+        isWinner(count)
+
+      }
+    }
+  }
+
+  case class Trainer() extends Person1
+
+  case class Gamer(num: Int) extends Person1
+
+}
+
+
+object UsingPerson extends Person1 with App {
+
+  val log = Logger.getLogger(this.getClass)
+  val totalAttendance = showPerson(Trainer())
+  log.info(s"$totalAttendance")
+  val count = 0
+  val result = showPerson(Gamer(count))
+  log.info(s"Result $result")
+
+}
